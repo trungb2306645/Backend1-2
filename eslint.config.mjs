67563ac15f7e3dc766c1f6e457ee7bca154650d3
Trungb2306645 <Trungb2306645@student.ctu.edu.vn>
2026-07-10 18:48:02 +0700

    Cau hinh Eslint du an

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,19 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node // ESLint hiểu các biến Node.js như 'process'
+      }
+    } 
+  },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { sourceType: "commonjs" } 
+  },
 ]);
